df4bed0c4d758c92870b888f9288e71abefd66ed
Bing6000s <thebing2002@outlook.com>
2025-05-02 07:06:53 -0400

    fix to vercel

--- bingnft/src/app/Profile/page.jsx
+++ bingnft/src/app/Profile/page.jsx
@@ -1,6 +1,6 @@
-import Navbar from "@/components/Navbar";
-import Sidebar from "@/components/Sidebar";
-import Profile from "@/components/Userpanel/profile"
+import Navbar from "./../../components/Navbar";
+import Sidebar from "./../../components/Sidebar";
+import Profile from "./../../components/Userpanel/profile"
 export default function Homepage() {
   return (
     <div className="min-h-screen bg-gray-100 overflow-hidden">
@@ -9,15 +9,15 @@ export default function Homepage() {
         <Sidebar />
 
         {/* Main content area */}
-        <main className="flex-1 flex flex-col px-6 py-4">
+        <main className="flex-1 flex flex-col">
           {/* Navbar aligned to the top-right */}
-          <div className="flex justify-end mb-4">
+          <div>
             <Navbar />
           </div>
 
           {/* Page heading */}
           <h1 className="text-center font-bold mb-6">
-            Edit Your NFT Card Information.
+            Add Your NFT Card Information.
           </h1>
 
           {/* Profile form aligned center-right */}
